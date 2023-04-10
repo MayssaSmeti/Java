c9253c3562ef1993ffff752bd23bf56f109b0e3f
ChahinezSahlaoui <chahinez.sahlaoui@esprit.tn>
2023-04-10 10:48:21 +0100

    reclamation

---
+++ src/edu/esprit/gui2/ReclamationController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.esprit.gui2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author msi
+ */
+public class ReclamationController implements Initializable {
+
+  
+    @FXML
+    private Label tfDescription;
+    @FXML
+    private Label tfObjet;
+    @FXML
+    private Label tfNote;
+
+    @FXML
+    private Button btnEnvoyer;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void saveReclamation(ActionEvent event) {
+    }
+    
+}
